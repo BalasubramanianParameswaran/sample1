79df21bf89618511b3c664802b26307ef7314e4f
balasubramanianparameswaran <balasubramanian.parameswaran@objectfrontier.com>
2014-11-01 11:21:59 +0530

    my first commit

---
+++ StaticDemo2.java
@@ -0,0 +1,23 @@
+package finalnstatic;
+
+public class StaticDemo2 {
+	public void abc() {
+		System.out.println("non static");
+	}
+
+	public static void abcd() {
+		System.out.println("static");
+	}
+
+	public static void main(String[] args) {
+
+		Thread.currentThread();
+
+		abcd();
+
+		StaticDemo2 sd = new StaticDemo2();
+		sd.abc();
+		sd.abcd();
+
+	}
+}
